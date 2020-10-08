9eaed725b2e38d321a891f385a105611c8633ad2
EC2 Default User <ec2-user@ip-172-31-79-9.ec2.internal>
2020-10-08 11:14:07 +0000

    4

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update] do
+    member do
+      get 'follows'
+      get 'followers'
+    end
+  end
   resources :books do
   resources :book_comments,only: [:create, :destroy]
   post 'favorite' => 'favorites#create', as: 'create_favorite'
@@ -8,4 +13,5 @@ Rails.application.routes.draw do
   end
   get '/home/about', to: 'homes#about'  #  => top.html.erb
   root :to => 'homes#top'
+  resources :relationships, only: [:create, :destroy]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_07_051758) do
+ActiveRecord::Schema.define(version: 2020_10_07_101242) do
 
   create_table "book_comments", force: :cascade do |t|
     t.integer "user_id"
@@ -37,6 +37,13 @@ ActiveRecord::Schema.define(version: 2020_10_07_051758) do
 
   create_table "post_images", force: :cascade do |t|
     t.string "image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follow_id"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
